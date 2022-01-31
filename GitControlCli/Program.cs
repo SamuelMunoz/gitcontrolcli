b01aeba7a7d46c6560bf8f74897ba37babfe764e
Samuel Muñoz <sdmunoz@gmail.com>
2022-01-31 14:46:39 -0600

    feat: initial commit

---
+++ GitControlCli/Program.cs
@@ -0,0 +1,31 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using Spectre.Console;
+
+await AnsiConsole.Status().Spinner(Spinner.Known.Dots)
+    .StartAsync("Fetching branches to remove...", async ctx =>
+    {
+        var fetch = await Cli.Wrap("git").WithArguments("remote update --prune").WithValidation(CommandResultValidation.None).ExecuteBufferedAsync();
+        
+        AnsiConsole.MarkupLine($"[red]{fetch.StandardError}[/]");
+        AnsiConsole.MarkupLine($"[green]Fetching branches completed[/]");
+        
+        ctx.Status("Cleaning local branches...");
+        ctx.Spinner(Spinner.Known.Clock);
+
+        var branch = await Cli.Wrap("powershell")
+            .WithArguments(@"-Command ""git branch --v | ? { $_ -match '\[gone\]' } | % { -split $_ | select -First 1 } | % { git branch -D $_ }"" ")
+            .WithValidation(CommandResultValidation.None).ExecuteBufferedAsync();
+        
+        AnsiConsole.MarkupLine($"[red]{branch.StandardError}[/]");
+        AnsiConsole.MarkupLine($"[green]Cleaning local branches completed[/]");
+        
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("[bold blue]Done![/]");
+    });
+
+
+
+
+
+
