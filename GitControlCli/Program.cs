b4dbf6e93df8b2b4beb3666c3e34659627f6ac46
Samuel Muñoz <sdmunoz@gmail.com>
2022-02-08 00:13:51 -0600

    feat: add cocona

--- GitControlCli/Program.cs
+++ GitControlCli/Program.cs
@@ -1,28 +1,38 @@
 ﻿using CliWrap;
 using CliWrap.Buffered;
+using Cocona;
 using Spectre.Console;
 
-await AnsiConsole.Status().Spinner(Spinner.Known.Dots)
-    .StartAsync("Fetching branches to remove...", async ctx =>
-    {
-        var fetch = await Cli.Wrap("git").WithArguments("remote update --prune").WithValidation(CommandResultValidation.None).ExecuteBufferedAsync();
-        
-        AnsiConsole.MarkupLine($"[red]{fetch.StandardError}[/]");
-        AnsiConsole.MarkupLine($"[green]Fetching branches completed[/]");
-        
-        ctx.Status("Cleaning local branches...");
-        ctx.Spinner(Spinner.Known.Clock);
+var builder = CoconaApp.CreateBuilder();
 
-        var branch = await Cli.Wrap("powershell")
-            .WithArguments(@"-Command ""git branch --v | ? { $_ -match '\[gone\]' } | % { -split $_ | select -First 1 } | % { git branch -D $_ }"" ")
-            .WithValidation(CommandResultValidation.None).ExecuteBufferedAsync();
+var app = builder.Build();
+
+app.AddCommand("run", async () =>
+{
+    await AnsiConsole.Status().Spinner(Spinner.Known.Dots)
+        .StartAsync("Fetching branches to remove...", async ctx =>
+        {
+            var fetch = await Cli.Wrap("git").WithArguments("remote update --prune").WithValidation(CommandResultValidation.None).ExecuteBufferedAsync();
         
-        AnsiConsole.MarkupLine($"[red]{branch.StandardError}[/]");
-        AnsiConsole.MarkupLine($"[green]Cleaning local branches completed[/]");
+            AnsiConsole.MarkupLine($"[red]{fetch?.StandardError}[/]");
+            AnsiConsole.MarkupLine($"[green]Fetching branches completed[/]");
         
-        AnsiConsole.WriteLine();
-        AnsiConsole.MarkupLine("[bold blue]Done![/]");
-    });
+            ctx.Status("Cleaning local branches...");
+            ctx.Spinner(Spinner.Known.Clock);
+
+            var branch = await Cli.Wrap("powershell")
+                .WithArguments(@"-Command ""git branch --v | ? { $_ -match '\[gone\]' } | % { -split $_ | select -First 1 } | % { git branch -D $_ }"" ")
+                .WithValidation(CommandResultValidation.None).ExecuteBufferedAsync();
+        
+            AnsiConsole.MarkupLine($"[red]{branch.StandardError}[/]");
+            AnsiConsole.MarkupLine($"[green]Cleaning local branches completed[/]");
+        
+            AnsiConsole.WriteLine();
+            AnsiConsole.MarkupLine("[bold blue]Done![/]");
+        });
+}).WithDescription("Runs the application");
+
+app.Run();
 
 
 
